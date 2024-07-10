0d8f610f90cfdb360c3f0c1565203ad29a5effa1
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-07-10 12:22:31 +0300

    modified  index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+// Function declaration for saturdayFun with default activity
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function expression for mondayWork with default activity
+  let mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Function that returns a function which wraps an adjective in a highlight
+  function wrapAdjective(flair = "*") {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+  };
+  
